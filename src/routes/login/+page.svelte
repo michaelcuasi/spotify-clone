73bdd6edddec087e373584a756f28458df2cfcb7
Michael <michaelcuasi@gmail.com>
2023-07-18 09:46:19 +0400

    initial commit

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,1 @@
+<a href="/api/auth/login">login</a>
---
+++ svelte.config.js
@@ -0,0 +1,17 @@
+import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	preprocess: preprocess({
+		scss: {
+			prependData: '@use "../styles/functions";@use "@unsass/breakpoint";'
+		}
+	}),
+
+	kit: {
+		adapter: adapter()
+	}
+};
+
+export default config;
